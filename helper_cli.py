c261f27d17c12ea53170d48425eaecc3ff7d58cd
Stas Ilinskiy <ilinskiy.sv@gmail.com>
2023-12-25 10:18:22 -0600

    implement colored output

--- helper_cli.py
+++ helper_cli.py
@@ -1,36 +1,59 @@
 import json
+from termcolor import colored
 
 SAVE_FILE_NAME_AUTO = "auto_save.json"
 SAVE_FILE_NAME_MANUAL = "manual_save.json"
 
 
 class Decks:
-    def __init__(self, infection: list[list[str]], discard: list[str]) -> None:
+    def __init__(
+        self,
+        infection: list[list[str]],
+        discard: list[str],
+        city_to_color: dict[str, str],
+    ) -> None:
         self.infection_deck: list[list[str]] = infection
         self.discard_pile: list[str] = discard
+        self.city_to_color: dict[str, str] = city_to_color
 
     @staticmethod
     def load(file_name: str) -> "Decks":
         with open(file_name, "rb") as file:
             obj = json.load(file)
-            infection = obj["infection"]
-            discard = obj["discard"]
-        return Decks(infection, discard)
+
+        return Decks(
+            obj.get("infection", []),
+            obj.get("discard", []),
+            obj.get("city_to_color", {}),
+        )
 
     def save(self, file_name: str) -> None:
         with open(file_name, "w") as file:
             json.dump(
-                {"infection": self.infection_deck, "discard": self.discard_pile},
+                {
+                    "infection": self.infection_deck,
+                    "discard": self.discard_pile,
+                    "city_to_color": self.city_to_color,
+                },
                 file,
                 indent=4,
             )
 
     def print(self) -> None:
-        print("[")
-        for x in self.infection_deck:
-            print("\t", x)
-        print("]")
-        print(self.discard_pile)
+        print("infection decks (topmost first):\n[")
+        for infections in self.infection_deck:
+            reprs = []
+            for city in infections:
+                reprs.append(self._format_city(city))
+            print(f"\t[{', '.join(reprs)}]")
+        print("]\n")
+        discards = [self._format_city(city) for city in self.discard_pile]
+        print(f"discard: [{', '.join(discards)}]")
+
+    def _format_city(self, city: str) -> str:
+        if city in self.city_to_color:
+            return colored(city, self.city_to_color[city])
+        return city
 
     def draw(self, card: str) -> None:
         if card in self.infection_deck[0]:
@@ -49,6 +72,12 @@ class Decks:
         if card in self.discard_pile:
             self.discard_pile.remove(card)
 
+    def mark_city(self, city: str, color: str) -> None:
+        self.city_to_color[city] = color
+
+    def unmark_city(self, city: str) -> None:
+        del self.city_to_color[city]
+
 
 def load_deck(file_name):
     with open(file_name, "r") as deck_file:
@@ -59,7 +88,7 @@ def mainloop(decks: Decks) -> None:
     user_option = ""
     while user_option != "q":
         user_option = input(
-            """\nquit, save, load, print, draw_card, shuffle, remove_discard:\n"""
+            """\nquit, save, load, print, draw_card, shuffle, remove_discard, mark_red, mark_yellow, unmark:\n"""
         )
         if user_option == "quit" or user_option == "q":
             decks.save(SAVE_FILE_NAME_AUTO)
@@ -110,11 +139,41 @@ def mainloop(decks: Decks) -> None:
                     decks.print()
             else:
                 print("usage: remove_discard|rd card_name")
+        elif user_option.startswith("mark_red") or user_option.startswith("mr"):
+            l = user_option.split(" ")
+            if len(l) >= 2:
+                for c in l[1:]:
+                    c = c.replace("_", " ").strip().lower()
+                    decks.mark_city(c, "red")
+                    decks.save(SAVE_FILE_NAME_AUTO)
+                decks.print()
+            else:
+                print("usage: mark_red|mr card_name")
+        elif user_option.startswith("mark_yellow") or user_option.startswith("my"):
+            l = user_option.split(" ")
+            if len(l) >= 2:
+                for c in l[1:]:
+                    c = c.replace("_", " ").strip().lower()
+                    decks.mark_city(c, "yellow")
+                    decks.save(SAVE_FILE_NAME_AUTO)
+                decks.print()
+            else:
+                print("usage: mark_yellow|my card_name")
+        elif user_option.startswith("unmark") or user_option.startswith("um"):
+            l = user_option.split(" ")
+            if len(l) >= 2:
+                for c in l[1:]:
+                    c = c.replace("_", " ").strip().lower()
+                    decks.unmark_city(c)
+                    decks.save(SAVE_FILE_NAME_AUTO)
+                decks.print()
+            else:
+                print("usage: unmark|um card_name")
 
 
 def main() -> None:
     start_deck = sorted(load_deck("infection_deck.txt"))
-    decks = Decks([start_deck], discard=[])
+    decks = Decks([start_deck], discard=[], city_to_color={})
     decks.print()
     mainloop(decks)
 
