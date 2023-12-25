be86a118c870acab833cc9471bdbcd6763d79d25
Stas Ilinskiy <ilinskiy.sv@gmail.com>
2023-12-24 23:49:14 -0600

    use json instead of pickle

--- helper_cli.py
+++ helper_cli.py
@@ -1,22 +1,30 @@
-import pickle
+import json
 
+SAVE_FILE_NAME_AUTO = "auto_save.json"
+SAVE_FILE_NAME_MANUAL = "manual_save.json"
 
 class Decks:
-    def __init__(self, infection, discard):
+    def __init__(self, infection: list[list[str]], discard: list[str]) -> None:
         self.infection_deck: list[list[str]] = infection
         self.discard_pile: list[str] = discard
 
     @staticmethod
-    def load(file_name):
-        with open(file_name, "rb") as pickle_file:
-            (infection, discard) = pickle.load(pickle_file)
+    def load(file_name: str) -> "Decks":
+        with open(file_name, "rb") as file:
+            obj = json.load(file)
+            infection = obj["infection"]
+            discard = obj["discard"]
         return Decks(infection, discard)
 
-    def save(self, file_name):
-        with open(file_name, "wb") as pickle_file:
-            pickle.dump((self.infection_deck, self.discard_pile), pickle_file)
+    def save(self, file_name: str) -> None:
+        with open(file_name, "w") as file:
+            json.dump(
+                {"infection": self.infection_deck, "discard": self.discard_pile},
+                file,
+                indent=4,
+            )
 
-    def print(self):
+    def print(self) -> None:
         print("[")
         for x in self.infection_deck:
             print("\t", x)
@@ -52,28 +60,28 @@ def mainloop(decks: Decks) -> None:
             """\nquit, save, load, print, draw_card, shuffle, remove_card:\n"""
         )
         if user_option == "quit" or user_option == "q":
-            decks.save("auto_save.pkl")
+            decks.save(SAVE_FILE_NAME_AUTO)
             break
         elif user_option == "save" or user_option == "s":
-            decks.save("manual_save.pkl")
+            decks.save(SAVE_FILE_NAME_MANUAL)
         elif user_option.startswith("load") or user_option.startswith("l "):
             l = user_option.split(" ")
             if len(l) == 1:
-                decks = Decks.load("manual_save.pkl")
+                decks = Decks.load(SAVE_FILE_NAME_MANUAL)
                 decks.print()
             elif len(l) == 2:
                 if l[1] == "manual":
-                    decks = Decks.load("manual_save.pkl")
+                    decks = Decks.load(SAVE_FILE_NAME_MANUAL)
                     decks.print()
                 elif l[1] == "auto":
-                    decks = Decks.load("auto_save.pkl")
+                    decks = Decks.load(SAVE_FILE_NAME_AUTO)
                     decks.print()
                 else:
                     print("usage: load|l [manual|auto]")
             else:
                 print("usage: load|l manual|auto")
         elif user_option == "print" or user_option == "p":
-            decks.save("auto_save.pkl")
+            decks.save(SAVE_FILE_NAME_AUTO)
             decks.print()
         elif user_option.startswith("draw_card") or user_option.startswith("dc"):
             l = user_option.split(" ")
@@ -81,21 +89,21 @@ def mainloop(decks: Decks) -> None:
                 for c in l[1:]:
                     c = c.replace("_", " ").strip().lower()
                     decks.draw(c)
-                    decks.save("auto_save.pkl")
+                    decks.save(SAVE_FILE_NAME_AUTO)
                     decks.print()
             else:
                 print("usage: draw_card|dc card_name")
         elif user_option.startswith("shuffle") or user_option.startswith("sh"):
             if len(decks.discard_pile) > 0:
                 decks.reshuffle_discard()
-                decks.save("auto_save.pkl")
+                decks.save(SAVE_FILE_NAME_AUTO)
             decks.print()
         elif user_option.startswith("remove_discard") or user_option.startswith("rd"):
             l = user_option.split(" ")
             if len(l) >= 2:
                 for c in l[1:]:
                     decks.remove_discard(c)
-                    decks.save("auto_save.pkl")
+                    decks.save(SAVE_FILE_NAME_AUTO)
                     decks.print()
             else:
                 print("usage: remove_discard|rd card_name")
