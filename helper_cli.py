91a57400c2bc1e26561e268ca63b26ca64834f6d
Stas Ilinskiy <ilinskiy.sv@gmail.com>
2023-12-24 23:20:15 -0600

    Create decks class

--- helper_cli.py
+++ helper_cli.py
@@ -1,18 +1,27 @@
 import pickle
 
-infection_deck: [[str]] = []
-discard_pile: [str] = []
 
+class Decks:
+    def __init__(self, infection, discard):
+        self.infection_deck: list[list[str]] = infection
+        self.discard_pile: list[str] = discard
 
-def save_state(state, file_name):
-    with open(file_name, "wb") as pickle_file:
-        pickle.dump(state, pickle_file)
+    def save(self, file_name):
+        with open(file_name, "wb") as pickle_file:
+            pickle.dump((self.infection_deck, self.discard_pile), pickle_file)
 
+    @staticmethod
+    def load(file_name):
+        with open(file_name, "rb") as pickle_file:
+            (infection, discard) = pickle.load(pickle_file)
+        return Decks(infection, discard)
 
-def load_state(file_name):
-    with open(file_name, "rb") as pickle_file:
-        res = pickle.load(pickle_file)
-    return res
+    def print(self):
+        print("[")
+        for x in self.infection_deck:
+            print("\t", x)
+        print("]")
+        print(self.discard_pile)
 
 
 def load_deck(file_name):
@@ -20,85 +29,69 @@ def load_deck(file_name):
         return [line.strip().lower() for line in deck_file.readlines()]
 
 
-def print_decks():
-    global infection_deck, discard_pile
-    print("[")
-    for x in infection_deck:
-        print("\t", x)
-    print("]")
-    print(discard_pile)
-
-
-def mainloop():
-    global infection_deck, discard_pile
+def mainloop(decks: Decks) -> None:
     user_option = ""
     while user_option != "q":
         user_option = input(
             """\nquit, save, load, print, draw_card, shuffle, remove_card:\n"""
         )
         if user_option == "quit" or user_option == "q":
-            save_state((infection_deck, discard_pile), "auto_save.pkl")
+            decks.save("auto_save.pkl")
             break
         elif user_option == "save" or user_option == "s":
-            save_state((infection_deck, discard_pile), "manual_save.pkl")
+            decks.save("manual_save.pkl")
         elif user_option.startswith("load") or user_option.startswith("l "):
             l = user_option.split(" ")
             if len(l) == 1:
-                infection_deck, discard_pile = load_state("manual_save.pkl")
-                print_decks()
+                decks = Decks.load("manual_save.pkl")
+                decks.print()
             elif len(l) == 2:
                 if l[1] == "manual":
-                    infection_deck, discard_pile = load_state("manual_save.pkl")
-                    print_decks()
+                    decks = Decks.load("manual_save.pkl")
+                    decks.print()
                 elif l[1] == "auto":
-                    infection_deck, discard_pile = load_state("auto_save.pkl")
-                    print_decks()
+                    decks = Decks.load("auto_save.pkl")
+                    decks.print()
                 else:
                     print("usage: load|l [manual|auto]")
             else:
                 print("usage: load|l manual|auto")
         elif user_option == "print" or user_option == "p":
-            save_state((infection_deck, discard_pile), "auto_save.pkl")
-            print_decks()
+            decks.save("auto_save.pkl")
+            decks.print()
         elif user_option.startswith("draw_card") or user_option.startswith("dc"):
             l = user_option.split(" ")
             if len(l) >= 2:
                 for c in l[1:]:
                     c = c.replace("_", " ").strip().lower()
-                    if c in infection_deck[0]:
-                        infection_deck[0].remove(c)
-                        if not infection_deck[0]:
-                            infection_deck.pop(0)
-                        discard_pile.append(c)
-                        discard_pile.sort()
-                        save_state(
-                            (infection_deck, discard_pile),
-                            "auto_save.pkl",
-                        )
-                        print_decks()
+                    if c in decks.infection_deck[0]:
+                        decks.infection_deck[0].remove(c)
+                        if not decks.infection_deck[0]:
+                            decks.infection_deck.pop(0)
+                        decks.discard_pile.append(c)
+                        decks.discard_pile.sort()
+                        decks.save("auto_save.pkl")
+                        decks.print()
                     else:
                         print("card not in infection deck", c)
             else:
                 print("usage: draw_card|dc card_name")
         elif user_option.startswith("shuffle") or user_option.startswith("sh"):
-            if len(discard_pile) > 0:
-                infection_deck.insert(0, discard_pile)
-                discard_pile = []
-                save_state((infection_deck, discard_pile), "auto_save.pkl")
-                print_decks()
+            if len(decks.discard_pile) > 0:
+                decks.infection_deck.insert(0, decks.discard_pile)
+                decks.discard_pile = []
+                decks.save("auto_save.pkl")
+                decks.print()
         elif user_option.startswith("remove_card") or user_option.startswith("rc"):
             l = user_option.split(" ")
             if len(l) == 3:
                 c = l[2].replace("_", " ").strip().lower()
                 if l[1].isdecimal:
                     i = int(l[1])
-                    if len(infection_deck) > i:
-                        infection_deck[i].remove(c)
-                        save_state(
-                            (infection_deck, discard_pile),
-                            "auto_save.pkl",
-                        )
-                        print_decks()
+                    if len(decks.infection_deck) > i:
+                        decks.infection_deck[i].remove(c)
+                        decks.save("auto_save.pkl")
+                        decks.print()
                     else:
                         print("that sub-deck doesn't exist")
                 else:
@@ -108,12 +101,10 @@ def mainloop():
 
 
 def main():
-    start_deck = load_deck("infection_deck.txt")
-    infection_deck.append(start_deck)
-
-    infection_deck[0].sort()
-    print_decks()
-    mainloop()
+    start_deck = sorted(load_deck("infection_deck.txt"))
+    decks = Decks([start_deck], discard=[])
+    decks.print()
+    mainloop(decks)
 
 
 if __name__ == "__main__":
