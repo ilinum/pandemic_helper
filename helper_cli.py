8ae1c5c88c99d1c387773f7dbab75f7e02f106ba
Stas Ilinskiy <ilinskiy.sv@gmail.com>
2023-12-26 21:33:58 -0600

    1. Fix sorting 2. Better output 3. Accept varargs

--- helper_cli.py
+++ helper_cli.py
@@ -3,6 +3,7 @@ import json
 import click
 from click.shell_completion import CompletionItem
 from termcolor import colored
+from functools import cmp_to_key
 
 SAVE_FILE_NAME = "save.json"
 STATE_FILE_NAME = "state.json"
@@ -45,24 +46,54 @@ class Decks:
                 indent=4,
             )
 
+    def _print_formatting(self, lis: [str]) -> [str]:
+        card_counts = {}
+        reprs = []
+        for card in lis:
+            card_counts[card] = card_counts.get(card, 0) + 1
+            # Sort by count, then name.
+            card_tuples = [(v, k) for k, v in card_counts.items()]
+            card_tuples.sort(key=cmp_to_key(self.compare))
+            reprs = []
+            for count, card in card_tuples:
+                name = f"x{count} " + self._format_name(card)
+                reprs.append(name)
+        return reprs
+
     def print(self) -> None:
         print("Infection decks (topmost first):")
         for i, deck in enumerate(self.infection_deck):
             if len(deck) == 0:
                 continue
-            card_counts = {}
-            for card in deck:
-                card_counts[card] = card_counts.get(card, 0) + 1
-            # Sort by count, then name.
-            card_tuples = [(v, k) for k, v in card_counts.items()]
-            card_tuples.sort()
-            reprs = []
-            for count, card in card_tuples:
-                name = self._format_name(card) + f" x{count}"
-                reprs.append(name)
-            print(f"{i+1}: {', '.join(reprs)}")
-        discards = [self._format_name(card) for card in self.discard_pile]
-        print(f"\nDiscard: [{', '.join(discards)}]")
+
+            deck_names = self._print_formatting(deck)
+            print(f"deck {i+1}: {len(deck)}")
+            for name in deck_names:
+                print(f"\t{name}")
+
+        discards = self._print_formatting(self.discard_pile)
+        print(f"\nDiscard: {len(self.discard_pile)}")
+        for name in discards:
+            print(f"\t{name}")
+
+    def compare(self, item1, item2):
+        count1 = item1[0]
+        count2 = item2[0]
+
+        if count1 > count2:
+            return -1
+
+        elif count2 > count1:
+            return 1
+        else:
+            name1 = item1[1]
+            name2 = item2[1]
+            if name1 > name2:
+                return 1
+            elif name2 > name1:
+                return -1
+            else:
+                return 0
 
     def _format_name(self, card: str) -> str:
         if card in self.card_to_color:
@@ -112,7 +143,7 @@ def cli():
 
 
 class CardNameType(click.ParamType):
-    name = "card"
+    name = "cards"
 
     def shell_complete(self, ctx, param, incomplete):
         # Implements shell completion for cities.
@@ -137,21 +168,23 @@ def _print() -> None:
 
 
 @cli.command("draw_card")
-@click.argument("card", type=CardNameType())
-def draw_card(card: str) -> None:
+@click.argument("cards", type=CardNameType(), nargs=-1)
+def draw_card(cards: [str]) -> None:
     decks = Decks.load(STATE_FILE_NAME)
-    card = card.replace("_", " ").strip().lower()
-    decks.draw(card)
+    for card in cards:
+        card = card.replace("_", " ").strip().lower()
+        decks.draw(card)
     decks.save(STATE_FILE_NAME)
     decks.print()
 
 
 @cli.command("remove_discard")
-@click.argument("card", type=CardNameType())
-def remove_discard(card: str) -> None:
+@click.argument("cards", type=CardNameType(), nargs=-1)
+def remove_discard(cards: list[str]) -> None:
     decks = Decks.load(STATE_FILE_NAME)
-    card = card.replace("_", " ").strip().lower()
-    decks.remove_discard(card)
+    for card in cards:
+        card = card.replace("_", " ").strip().lower()
+        decks.remove_discard(card)
     decks.save(STATE_FILE_NAME)
     decks.print()
 
@@ -166,14 +199,15 @@ def shuffle() -> None:
 
 @cli.command()
 @click.option("--color", "-c", required=True, help="red|yellow|none")
-@click.argument("card", type=CardNameType())
-def mark(color: str, card: str) -> None:
+@click.argument("cards", type=CardNameType(), nargs=-1)
+def mark(color: str, cards: list[str]) -> None:
     decks = Decks.load(STATE_FILE_NAME)
-    card = card.replace("_", " ").strip().lower()
-    if color.lower() == "none":
-        decks.unmark_card(card)
-    else:
-        decks.mark_card(card, color)
+    for card in cards:
+        card = card.replace("_", " ").strip().lower()
+        if color.lower() == "none":
+            decks.unmark_card(card)
+        else:
+            decks.mark_card(card, color)
     decks.save(STATE_FILE_NAME)
     decks.print()
 
